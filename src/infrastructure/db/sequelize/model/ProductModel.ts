aab78d0ca894ff2e061e8feb3da827e1a62f6c66
Tiago Cardoso <tiago.mncardoso@gmail.com>
2022-10-06 08:08:39 -0300

    chore: added sequelize orm

---
+++ src/infrastructure/db/sequelize/model/ProductModel.ts
@@ -0,0 +1,20 @@
+import { PrimaryKey } from 'sequelize-typescript';
+import { Column } from 'sequelize-typescript';
+import { Table } from 'sequelize-typescript';
+import { Model } from 'sequelize-typescript'
+
+@Table({
+    tableName: 'products',
+    timestamps: false,
+})
+export default class ProductModel extends Model {
+    @PrimaryKey
+    @Column
+    declare id: string
+
+    @Column({allowNull: false})
+    declare name: string
+
+    @Column({allowNull: false})
+    declare price: number
+}
